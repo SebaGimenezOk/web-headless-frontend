8ec69cb106531f3503145de65cd5378d37416a97
Seba Gimenez <sebagimenez@MacBook-Air-de-Seba.local>
2025-12-19 18:44:30 -0300

    Define initial content model for headless WordPress

---
+++ src/lib/contentModel.js
@@ -0,0 +1,28 @@
+export const contentModel = {
+  post: {
+    title: "",
+    slug: "",
+    excerpt: "",
+    content: "",
+    featured_image: "",
+    date: "",
+    author: "",
+    categories: [],
+    tags: [],
+  },
+  page: {
+    title: "",
+    slug: "",
+    content: "",
+  },
+  podcast: {
+    title: "",
+    slug: "",
+    description: "",
+    audio_url: "",
+    cover_image: "",
+    author: "",
+    duration: "",
+    publish_date: "",
+  },
+};

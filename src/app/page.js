8aa550b013b1fdd68e87778487a7b5cc81e4b853
Seba Gimenez <sebagimenez@MacBook-Air-de-Seba.local>
2025-12-22 18:37:46 -0300

    clonacion para solucionar tailwind

--- src/app/page.js
+++ src/app/page.js
@@ -1,12 +1,35 @@
-export default function Home() {
-  return (
-    <section className="max-w-7xl mx-auto px-6 py-16">
-      <h1 className="text-3xl font-bold mb-4">
-        Proyecto Next + WordPress Headless
-      </h1>
-      <p className="text-gray-600">
-        Base lista para comenzar a consumir contenido dinámico.
-      </p>
-    </section>
-  );
+"use client";
+
+import { useEffect, useState } from "react";
+import PodcastList from "../components/PodcastList";
+
+export default function Page() {
+  const [posts, setPosts] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState("");
+
+  useEffect(() => {
+    const fetchPosts = async () => {
+      try {
+        const res = await fetch("http://localhost:3000/api/posts");
+
+        if (!res.ok) throw new Error(`HTTP error! status: ${res.status}`);
+        const data = await res.json();
+        setPosts(data);
+      } catch (err) {
+        console.error("Error al obtener posts:", err);
+        setError("Error al obtener posts");
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchPosts();
+  }, []);
+
+  if (loading) return <div>Cargando podcasts...</div>;
+  if (error) return <div>{error}</div>;
+  if (posts.length === 0) return <div>No hay podcasts disponibles</div>;
+
+  return <PodcastList posts={posts} />;
 }

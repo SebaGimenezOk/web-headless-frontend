1a1c64911358b9779d64b937e4dfd97f9b36de28
Seba Gimenez <sebagimenez@MacBook-Air-de-Seba.local>
2026-03-26 00:17:40 -0300

    Fixed url, optimizado para img & listo para redeploy

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,7 @@ const nextConfig = {
   output: "export",
 
   images: {
+    unoptimized: true, // <--- esto desactiva la optimización que falla en export
     domains: ["www.cronicasdeunespectador.com"],
   },
 };

31f1c70fadc718cada5c8141bf96bbd67e668c69
Seba Gimenez <sebagimenez@MacBook-Air-de-Seba.local>
2026-03-26 23:58:16 -0300

    fix api url production

--- src/lib/wordpress.js
+++ src/lib/wordpress.js
@@ -1,6 +1,6 @@
 // src/lib/wordpress.js
 
-const API_URL = "http://web-headless.local/wp-json/wp/v2";
+const API_URL = "https://api.cronicasdeunespectador.com/wp-json/wp/v2";
 
 export async function getPodcasts() {
   const res = await fetch(`${API_URL}/podcast`, {

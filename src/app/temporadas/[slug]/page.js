8debd3a0f225e9af3f37260d06ca5a34ae4d905d
Seba Gimenez <sebagimenez@MacBook-Air-de-Seba.local>
2026-05-23 18:28:38 -0300

    ajuste navbar3

--- src/app/temporadas/[slug]/page.js
+++ src/app/temporadas/[slug]/page.js
@@ -1,7 +1,12 @@
 import { getTemporadaBySlug } from "@/services/taxonomies";
+import { getPodcastsByTemporadaId } from "@/services/podcasts";
 
 export default async function TemporadaPage({ params }) {
+  console.log("PARAMS:", params);
+
   const temporada = await getTemporadaBySlug(params.slug);
+
+  console.log("TEMPORADA:", temporada);
 
   if (!temporada) {
     return (
@@ -16,19 +21,22 @@ export default async function TemporadaPage({ params }) {
 
   const podcasts = await getPodcastsByTemporadaId(temporada.id);
 
-
   return (
-    <div>
-      <h1>{temporada.name}</h1>
+    <div className="p-6">
+      <h1 className="text-2xl font-bold mb-4">
+        {temporada.name}
+      </h1>
 
       {podcasts.length === 0 ? (
         <p>No hay podcasts</p>
       ) : (
-        podcasts.map((p) => (
-          <div key={p.id}>
-            {p.title}
-          </div>
-        ))
+        <div className="grid gap-4">
+          {podcasts.map((p) => (
+            <div key={p.id} className="border p-3">
+              {p.title}
+            </div>
+          ))}
+        </div>
       )}
     </div>
   );

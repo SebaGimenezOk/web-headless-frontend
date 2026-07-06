90f62e8c10001097cf2e9e2001adf23c2e62733d
Seba Gimenez <sebagimenez@MacBook-Air-de-Seba.local>
2026-07-06 20:19:03 -0300

    ajsute tono

--- src/components/PaperBackground.jsx
+++ src/components/PaperBackground.jsx
@@ -20,7 +20,7 @@ export default function PaperBackground({ children }) {
           width="100%"
           height="100%"
           colorBack="#faf9f6" 
-          colorFront="##f1f1f1" // Grano suave
+          colorFront="#f1f1f1" // Grano suave
           contrast={0.02} // CLAVE: Al mínimo para que no haga el manchón gris en el centro
           roughness={0.6} // Grano muy fino y sutil
           fiber={0.4} // fibras

18ef6e5a06543bbf21c501c8ab536301fdcf2de9
Seba Gimenez <sebagimenez@MacBook-Air-de-Seba.local>
2026-07-06 17:53:28 -0300

    style: ajustar textura 2l

--- src/components/PaperBackground.jsx
+++ src/components/PaperBackground.jsx
@@ -13,17 +13,16 @@ export default function PaperBackground({ children }) {
         width: '100vw',
         height: '100vh',
         zIndex: -1, 
-        pointerEvents: 'none' // Clave para que el usuario pueda hacer click y seleccionar texto sin problemas
+        pointerEvents: 'none' 
       }}>
         <PaperTexture 
           width="100%" 
           height="100%" 
-          fit="cover" // Hace que la imagen use toda la pantalla como lienzo único sin repetirse
-          image="/TexturaPapelCanson1.jpg" // Nombre exacto con el que guardaste la imagen en public/
+
           colorBack="#ffffff" 
           colorFront="#f5f5f5" // Color crema/tinta suave para darle calidez a las fibras del relieve
-          contrast={0.04}      // Mantenelo sutil para que no compita con los textos de tus críticas
-          roughness={0.06}     // Textura granulada fina táctil
+          contrast={0.01}      // Mantenelo sutil para que no compita con los textos de tus críticas
+          roughness={0.02}     // Textura granulada fina táctil
           fiber={0.1}          // Fibras de papel tenues
           fiberSize={0.2}
           crumples={0.05}       // Arrugas de papel muy suavecitas

043d257184bfa23ca239c98568bfd2e6555e7099
Seba Gimenez <sebagimenez@MacBook-Air-de-Seba.local>
2026-07-07 19:32:45 -0300

    biografia con foto 3

--- src/components/BioContent.jsx
+++ src/components/BioContent.jsx
@@ -11,13 +11,13 @@ export default function BioContent() {
           alt="Foto del Autor"
           fill
           sizes="(max-width: 1024px) 100vw, 896px"
-          className="object-cover m-0"
+          className="object-cover"
           priority
         />
       </div>
 
       {/* Bloque de Texto Debajo (Casi ancho completo y con tu tipografía) */}
-      <div className="w-[95%] px-40 space-y-4 text-justify">
+      <div className="w-[95%] px-4 space-y-4 text-justify">
         <p className="text-xs tracking-wider font-medium text-(--text-strong) uppercase leading-relaxed">
           Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin ac diam id lorem tincidunt 
           consequat. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; 

dbf2e10df29b953faa8cec4764e879a24a90ac81
Seba Gimenez <sebagimenez@MacBook-Air-de-Seba.local>
2026-07-07 20:49:50 -0300

    hero 6

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -13,7 +13,7 @@ export default function Hero() {
       <section className="relative w-full h-[60vh] overflow-hidden bg-(--background)">
         {/* Imagen de fondo con opacidad reducida para mejorar legibilidad */}
         <Image
-          src="/SalaColonExtendida.jpg"
+          src="/AmaduesPerfil.png"
           alt="Wolfgang Amadeus Mozart"
           fill
           priority

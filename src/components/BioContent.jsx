26f74665a89911a28ce545ca6733a3902274efab
Seba Gimenez <sebagimenez@MacBook-Air-de-Seba.local>
2026-07-07 19:31:10 -0300

    biografia con foto 2

--- src/components/BioContent.jsx
+++ src/components/BioContent.jsx
@@ -5,7 +5,7 @@ export default function BioContent() {
     <div className="w-full max-w-4xl mx-auto flex flex-col gap-6">
       
       {/* Imagen Horizontal Arriba (Formato apaisado nítido) */}
-      <div className="relative w-full aspect-16/7 sm:aspect-21/9 overflow-hidden">
+      <div className="relative w-full aspect-video sm:aspect-21/9 overflow-hidden">
         <Image
           src="/benavides 3.png"
           alt="Foto del Autor"
@@ -17,7 +17,7 @@ export default function BioContent() {
       </div>
 
       {/* Bloque de Texto Debajo (Casi ancho completo y con tu tipografía) */}
-      <div className="w-[95%] mx-auto space-y-4 text-justify">
+      <div className="w-[95%] px-40 space-y-4 text-justify">
         <p className="text-xs tracking-wider font-medium text-(--text-strong) uppercase leading-relaxed">
           Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin ac diam id lorem tincidunt 
           consequat. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; 

bc00c970ffd28f90623d79d86b417e4b0c3bfba3
Seba Gimenez <sebagimenez@MacBook-Air-de-Seba.local>
2026-07-10 20:55:29 -0300

    fix css 4

--- src/components/ProgramasList.jsx
+++ src/components/ProgramasList.jsx
@@ -24,7 +24,7 @@ export default async function ProgramasList() {
             {/* IMAGEN Fija en 180x180 */}
             <div className="relative w-45 h-45 shrink-0 overflow-hidden rounded-2xl">
               <Image
-                src="/benavides2.png" 
+                src="/benavidesfantasia2.png" 
                 alt="Conductores"
                 width={180}
                 height={180}

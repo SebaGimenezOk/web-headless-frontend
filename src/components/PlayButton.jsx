7e751eff13fa6d6b3a466e4def3d3fcdb1814eb8
Seba Gimenez <sebagimenez@MacBook-Air-de-Seba.local>
2026-08-18 18:22:03 -0300

    fix:button audio 5

--- src/components/PlayButton.jsx
+++ src/components/PlayButton.jsx
@@ -12,7 +12,7 @@ export default function PlayButton({ url, label = "▶ Reproducir" }) {
   return (
     <button
       onClick={handleClick}
-      className= "inline-flex  items-center justify-center gap-2 w-full py-2.5 px-4 bg-[#f8f5ee] hover:bg-[#efece4] text-neutral-800 border border-[#e5e0d8] text-xs uppercase tracking-wider transition-colors rounded-xl font-medium shadow-sm"
+      className= "inline-flex cursor-pointer items-center justify-center gap-2 w-full py-2.5 px-4 bg-[#f8f5ee] hover:bg-[#efece4] text-neutral-800 border border-[#e5e0d8] text-xs uppercase tracking-wider transition-colors rounded-xl font-medium shadow-sm"
        >
       {label}
     </button>

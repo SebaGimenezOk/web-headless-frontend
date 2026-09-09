96bba5ab2649371d777db09fa5f83b963cb4acb1
Seba Gimenez <sebagimenez@MacBook-Air-de-Seba.local>
2026-09-09 15:20:59 -0300

    fix: size logo banners 2

--- src/components/InstitutionalLinks.jsx
+++ src/components/InstitutionalLinks.jsx
@@ -23,8 +23,8 @@ export default function InstitutionalLinks() {
               <Image
                 src={site.logo}
                 alt={site.name}
-                width={360}
-                height={140}
+                width={200}
+                height={200}
                 className="h-28 w-auto opacity-85 transition-all duration-300 hover:opacity-100 hover:-translate-y-1"
               />
             </Link>

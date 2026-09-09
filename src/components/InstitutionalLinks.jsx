1a81caba4ac46888e8e361aee5427d97049f4334
Seba Gimenez <sebagimenez@MacBook-Air-de-Seba.local>
2026-09-09 15:23:05 -0300

    fix: size logo banners 3

--- src/components/InstitutionalLinks.jsx
+++ src/components/InstitutionalLinks.jsx
@@ -10,7 +10,7 @@ export default function InstitutionalLinks() {
           Instituciones y medios de referencia
         </h2>
 
-        <div className="flex flex-wrap items-center justify-center gap-x-24 gap-y-12">
+        <div className="flex flex-wrap items-center justify-center gap-x-20 gap-y-12">
           {institutionalLinks.map((site) => (
             <Link
               key={site.name}
